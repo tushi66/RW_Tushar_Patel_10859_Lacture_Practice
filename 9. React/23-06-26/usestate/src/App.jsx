9fa26b7e9626526c922b6447ccbbdff86bf97fa1
Tushar Patel <pateltusharsa@gmail.com>
2026-06-23 15:06:54 +0530

    Add React project files

---
+++ 9. React/23-06-26/usestate/src/App.jsx
@@ -0,0 +1,21 @@
+import './App.css'
+import HookEffect from './useeffect'
+import Apicall from './api'
+
+function App() {
+
+  return(
+
+    <>
+    <h1>Hiii... Welocome to The Page...</h1>
+
+    <HookEffect />
+
+    <Apicall />
+    
+    </>
+  )
+
+}
+
+export default App
